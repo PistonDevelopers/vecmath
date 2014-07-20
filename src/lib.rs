a31fa3239d36e2e3c02dcf5e2ee8252785bd579f
Sven Nilsen <bvssvni@gmail.com>
2014-07-20 20:36:11 +0200

    Added `mat3_id`
    Closes https://github.com/PistonDevelopers/vecmath/issues/122

--- src/lib.rs
+++ src/lib.rs
@@ -145,6 +145,18 @@ pub fn mat3x2_id<T: One + Zero + Copy>() -> Matrix3x2<T> {
     ]
 }
 
+/// Constructs identity matrix.
+#[inline(always)]
+pub fn mat3_id<T: One + Zero + Copy>() -> Matrix3<T> {
+    let one = One::one();
+    let zero = Zero::zero();
+    [
+        [one, zero, zero],
+        [zero, one, zero],
+        [zero, zero, one]
+    ]
+}
+
 /// Converts to a f32 vector.
 #[inline(always)]
 pub fn vec2_to_f32<T: ToPrimitive>(a: Vector2<T>) -> Option<Vector2<f32>> {
