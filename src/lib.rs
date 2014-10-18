59396cc907f78a56dd080e4c3dec3a5f92b759de
Sven Nilsen <bvssvni@gmail.com>
2014-10-18 19:45:08 +0200

    Removed duplicate code
    Closes https://github.com/PistonDevelopers/vecmath/issues/197

--- src/lib.rs
+++ src/lib.rs
@@ -1031,24 +1031,6 @@ pub fn mat4_add<T: Num + Copy>(a: Matrix4<T>, b: Matrix4<T>) -> Matrix4<T> {
     ]
 }
 
-/// Multiplies vector with scalar.
-#[inline(always)]
-pub fn vec2_mul_scalar<T: Num>(a: Vector2<T>, b: T) -> Vector2<T> {
-    [a[0] * b, a[1] * b]
-}
-
-/// Multiplies vector with scalar.
-#[inline(always)]
-pub fn vec3_mul_scalar<T: Num>(a: Vector3<T>, b: T) -> Vector3<T> {
-    [a[0] * b, a[1] * b, a[2] * b]
-}
-
-/// Multiplies vector with scalar.
-#[inline(always)]
-pub fn vec4_mul_scalar<T: Num>(a: Vector4<T>, b: T) -> Vector4<T> {
-    [a[0] * b, a[1] * b, a[2] * b, a[3] * b]
-}
-
 /// Multiplies two vectors component wise.
 #[inline(always)]
 pub fn vec2_mul<T: Num>(a: Vector2<T>, b: Vector2<T>) -> Vector2<T> {
